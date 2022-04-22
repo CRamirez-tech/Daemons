520208e14e29f15d8f6785e4e3d94f9e5951a022
cnameless <52332890+CRamirez-tech@users.noreply.github.com>
2022-04-22 09:51:26 -0500

    first change-fail

--- DaemonsTest.pro
+++ DaemonsTest.pro
@@ -2,21 +2,18 @@ QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-CONFIG += c++17
+CONFIG += c++20
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    main.cpp \
-    mainwindow.cpp
+    main.cpp
 
-HEADERS += \
-    mainwindow.h
+HEADERS +=
 
-FORMS += \
-    mainwindow.ui
+FORMS +=
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
-#include "mainwindow.h"
+// thread example
+#include <iostream>
+#include <thread>
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
+void foo()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    for(int i=0;i<10;i++){
+        std::cout<<"This is foo."<<std::endl;
+    }
+}
+
+void bar(int x)
+{
+    for(int i=0;i<10;i++){
+        std::cout<<x<<std::endl;
+    }
+}
+
+int main()
+{
+    std::thread first (foo);     // spawn new thread that calls foo()
+    std::thread second (bar,0);  // spawn new thread that calls bar(0)
+
+    std::cout << "main, foo and bar now execute concurrently...\n";
+
+    // synchronize threads:
+    first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
+
+    std::cout << "foo and bar completed.\n";
+
+    return 0;
 }
